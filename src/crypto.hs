c9348eb22aaff6f20984c811f186d114ff4c0ec8
smallka <iamsmallka@gmail.com>
2012-06-03 22:54:23 +0800

    add hash table for mpq

---
+++ src/crypto.hs
@@ -0,0 +1,21 @@
+import Data.Word
+import Data.List
+import Data.Bits
+
+build1D :: Word32 -> Word32 -> Word32 -> [ Word32 ]
+build1D 0 q r = []
+build1D len q r = seed : (build1D (len - 1) new_q new_r)
+    where mid_r = (r * 125 + 3) `mod` 0x2AAAAB
+          mid_seed = (mid_r .&. 0xFFFF) `shiftL` 16
+          (new_q, new_r) = divMod (mid_r * 125 + 3) 0x2AAAAB
+          seed = mid_seed .|. (new_r .&. 0xFFFF)
+
+split2D :: [ Word32 ] -> [ [ Word32 ] ]
+split2D [] = []
+split2D all = first_row : (split2D left)
+    where (first_row, left) = splitAt 5 all
+
+hashTable :: [ Word32 ]
+hashTable = concat $ transpose $ split2D $ build1D 0x500 0x100001 0x100001
+
+
