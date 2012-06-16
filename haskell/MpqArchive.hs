a3f45991f212d8e9fd285525abd4b1a25ca52caf
smallka <iamsmallka@gmail.com>
2012-06-16 21:23:58 +0800

    simplify parsing by supporting Version 4 only

--- haskell/MpqArchive.hs
+++ haskell/MpqArchive.hs
@@ -13,30 +13,21 @@ import Crypto (hash, decrypt)
 data MpqHeader = MpqHeader {
     headerSize :: Word,
     archiveDataLength :: Word,
-    mpqVersion :: Word,
+    version :: Word,
     blockSize :: Word,
     hashTableOffset :: Word,
     blockTableOffset :: Word,
     hashTableLength :: Word,
     blockTableLength :: Word,
-    v2 :: Maybe MpqHeaderV2
-} deriving (Show)
-
-data MpqHeaderV2 = MpqHeaderV2 {
+    -- Version 2 (Burning Crusade)
     highBlockTableOffset :: Word,
     hashTableOffsetHigh :: Word,
     blockTableOffsetHigh :: Word,
-    v3 :: Maybe MpqHeaderV3
-} deriving (Show)
-
-data MpqHeaderV3 = MpqHeaderV3 {
+    -- Version 3 (Cataclysm First)
     archiveDataLength64 :: Word,
     ebtOffset :: Word,
     ehtOffset :: Word,
-    v4 :: Maybe MpqHeaderV4
-} deriving (Show)
-
-data MpqHeaderV4 = MpqHeaderV4 {
+    -- Version 4 (Cataclysm Second)
     hashTableCompressedSize :: Word,
     blockTableCompressedSize :: Word,
     highBlockTableCompressedSize :: Word,
@@ -60,84 +51,45 @@ parseHeader = do
 
     headerSize <- gw32
     archiveDataLength <- gw32
-    mpqVersion <- gw16
+    version <- gw16
+    when (version /= 3) $ fail "only support version 4 (Cataclysm Second)"
+
     blockSize <- gw16 >>= return . (shift 0x200) . fromIntegral
     hashTableOffset <- gw32
     blockTableOffset <- gw32
     hashTableLength <- gw32
     blockTableLength <- gw32
 
-    v2 <- if mpqVersion >= 1
-        then parseV2 mpqVersion >>= return . Just
-        else return Nothing
-
-    return $ MpqHeader headerSize archiveDataLength mpqVersion blockSize hashTableOffset blockTableOffset hashTableLength blockTableLength v2
-
-parseV2 :: Word -> Get MpqHeaderV2
-parseV2 mpqVersion = do
     highBlockTableOffset <- gw64
     hashTableOffsetHigh <- gw16
     blockTableOffsetHigh <- gw16
 
-    v3 <- if mpqVersion >= 2
-        then parseV3 mpqVersion >>= return . Just
-        else return Nothing
-
-    return $ MpqHeaderV2 highBlockTableOffset hashTableOffsetHigh blockTableOffsetHigh v3
-
-parseV3 :: Word -> Get MpqHeaderV3
-parseV3 mpqVersion = do
     archiveDataLength64 <- gw64
     ebtOffset <- gw64
     ehtOffset <- gw64
 
-    v4 <- if mpqVersion >= 3
-        then parseV4 >>= return . Just
-        else return Nothing
-
-    return $ MpqHeaderV3 archiveDataLength64 ebtOffset ehtOffset v4
-
-parseV4 :: Get MpqHeaderV4
-parseV4 = do
     hashTableCompressedSize <- gw64
     blockTableCompressedSize <- gw64
     highBlockTableCompressedSize <- gw64
     ebtCompressedSize <- gw64
     ehtCompressedSize <- gw64
     rawChunkSize <- gw32
+
     skip $ 6 * 16 -- hashes
-    return $ MpqHeaderV4 hashTableCompressedSize blockTableCompressedSize highBlockTableCompressedSize ebtCompressedSize ehtCompressedSize rawChunkSize
+    return $ MpqHeader headerSize archiveDataLength version blockSize hashTableOffset blockTableOffset hashTableLength blockTableLength highBlockTableOffset hashTableOffsetHigh blockTableOffsetHigh archiveDataLength64 ebtOffset ehtOffset hashTableCompressedSize blockTableCompressedSize highBlockTableCompressedSize ebtCompressedSize ehtCompressedSize rawChunkSize
 
 checkHeader :: MpqHeader -> IO ()
 checkHeader header = do
     when (hashTableLength header * 16 /= (blockTableOffset header) - (hashTableOffset header)) $ fail "compressed hash table not supported"
     when (blockTableLength header * 16 /= (archiveDataLength header) - (blockTableOffset header)) $ fail "compressed block table not supported"
-    case v2 header of
-        Just headerV2 -> checkV2 header headerV2
-        Nothing -> return ()
+    when (highBlockTableOffset header /= 0) $ fail "Hi-Block Table not supported"
+    when (hashTableOffsetHigh header /= 0) $ fail "hash Table High bit not supported"
+    when (blockTableOffsetHigh header /= 0) $ fail "block Table High bit not supported"
+    when (archiveDataLength64 header /= archiveDataLength header) $ fail "64-bit archive size not supported"
+    when (hashTableCompressedSize header /= (hashTableLength header * 16)) $ fail "compressed hash table not supported"
+    when (blockTableCompressedSize header /= (blockTableLength header * 16)) $ fail "compressed block table not supported"
+    when (highBlockTableCompressedSize header /= 0) $ fail "Hi-Block Table not supported"
     -- TODO: check for strong signature presence
-
-checkV2 :: MpqHeader -> MpqHeaderV2 -> IO ()
-checkV2 base v2 = do
-    when (highBlockTableOffset v2 /= 0) $ fail "Hi-Block Table not supported"
-    when (hashTableOffsetHigh v2 /= 0) $ fail "hash Table High bit not supported"
-    when (blockTableOffsetHigh v2 /= 0) $ fail "block Table High bit not supported"
-    case v3 v2 of
-        Just headerV3 -> checkV3 base headerV3
-        Nothing -> return ()
-
-checkV3 :: MpqHeader -> MpqHeaderV3 -> IO ()
-checkV3 base v3 = do
-    when (archiveDataLength64 v3 /= archiveDataLength base) $ fail "64-bit archive size not supported"
-    case v4 v3 of
-        Just headerV4 -> checkV4 base headerV4
-        Nothing -> return ()
-
-checkV4 :: MpqHeader -> MpqHeaderV4 -> IO ()
-checkV4 base v4 = do
-    when (hashTableCompressedSize v4 /= (hashTableLength base * 16)) $ fail "compressed hash table not supported"
-    when (blockTableCompressedSize v4 /= (blockTableLength base * 16)) $ fail "compressed block table not supported"
-    when (highBlockTableCompressedSize v4 /= 0) $ fail "Hi-Block Table not supported"
 
 data MpqHashEntry = MpqHashEntry {
     hashA :: Word,
