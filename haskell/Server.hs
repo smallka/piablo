af4cac91094ab517f75d40ffb9997cbdaca915bb
smallka <iamsmallka@gmail.com>
2012-06-24 01:06:36 +0800

    D3 proto parse

--- haskell/Server.hs
+++ haskell/Server.hs
@@ -1,20 +1,44 @@
 {-# LANGUAGE OverloadedStrings #-}
 
+import Data.Word
 import Data.Conduit
 import Data.Conduit.Network
-import Data.Conduit.Binary as CB
-import Data.Bits
-import Data.ByteString as S
-import Data.ByteString.Char8 as S8
-import Data.Binary.Get (runGet, getWord32be)
+import qualified Data.Conduit.Binary as CB
+import qualified Data.ByteString as S
+import qualified Data.ByteString.Char8 as S8
+import qualified Data.Binary.Get as G
+import qualified D3BitGet as BG
 import System.IO (stdout)
+import Control.Applicative ((<$>), (<*>))
+
+data Message = JoinBNetGame Word64 Word64 Word64 Word64 Word64 Word32 Word64 Word8 Word32 Word32
+    | Unknown Int
+    deriving (Show)
+
+parseMessage :: BG.BitGet Message
+parseMessage = do
+    proto <- fromIntegral <$> BG.getWord16be 9
+    case proto of
+        10 -> JoinBNetGame <$>
+            BG.getWord64be 64 <*>
+            BG.getWord64be 64 <*>
+            BG.getWord64be 64 <*>
+            BG.getWord64be 64 <*>
+            BG.getWord64be 64 <*>
+            BG.getWord32be 32 <*>
+            BG.getWord64be 64 <*>
+            BG.getWord8 4 <*>
+            BG.getWord32be 32 <*>
+            BG.getWord32be 32
+        otherwise -> return $ Unknown proto
 
 handlePacket :: Resource m => Conduit S.ByteString m S.ByteString
 handlePacket = sequenceSink () $ \() -> do
     bytes <- CB.take 4
-    let size = fromIntegral $ runGet getWord32be bytes
+    let size = fromIntegral $ G.runGet G.getWord32be bytes
     content <- CB.take (size - 4)
-    return $ Emit () [S8.pack $ show size]
+    let msg = G.runGet (BG.runBitGet parseMessage) content
+    return $ Emit () [S8.pack $ show msg]
 
 main :: IO ()
 main = runTCPServer (ServerSettings 1999 $ Just "127.0.0.1") $
