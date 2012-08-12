530f7e5c754122ef13ccb209e914412f6776682f
smallka <iamsmallka@gmail.com>
2012-08-12 23:11:09 +0800

    Parse JoinBNetGameMessage

--- python/middle_man.py
+++ python/middle_man.py
@@ -5,6 +5,9 @@ from twisted.internet.protocol import Factory
 from twisted.internet.endpoints import TCP4ServerEndpoint
 from twisted.internet import reactor
 
+from bitstring import BitStream
+from bit_reader import BitReader
+
 dest_ip = None
 dest_port = None
 
@@ -12,6 +15,50 @@ client_protocol = None
 server_protocol = None
 
 c2s_queue = []
+
+def EntityId(reader):
+	print "EntityId", reader.read_int64(64), reader.read_int64(64)
+
+def GameId(reader):
+	print "GameId", reader.read_int64(64), reader.read_int64(64), reader.read_int64(64)
+
+def JoinBNetGameMessage(reader):
+	EntityId(reader)
+	GameId(reader)
+	print reader.read_int(32)
+	print reader.read_int64(64)
+	print reader.read_int(4)
+	print "ProtoHash", reader.read_int(32)
+	print "SnoPackHash", reader.read_int(32)
+
+
+g_opcodes = {
+	10 : JoinBNetGameMessage, 
+}
+
+def parseData(data):
+	while True:
+		if len(data) < 4:
+			break
+		size = BitStream(bytes=data[:4]).uint
+		if len(data) < size:
+			print "ERROR: incomplete packet"
+
+		reader = BitReader(data[4:size])
+		while reader.get_bit_len() >= 9:
+			opcode = reader.read_int(9)
+			if opcode not in g_opcodes:
+				print "unrecognized opcode", opcode
+				break
+			else:
+				func = g_opcodes[opcode]
+				print "[%d] %s" % (opcode, func.__name__)
+				func(reader)
+
+		data = data[size:]
+
+	if len(data) > 0:
+		print "ERROR: %d bytes unrecognized", len(data)
 
 class DestAddressProtocol(DatagramProtocol):
 
@@ -43,6 +90,7 @@ class ClientProtocol(Protocol):
 			print "postpone client send (len=%d)" % len(data)
 			c2s_queue.append(data)
 		else:
+			parseData(data)
 			server_protocol.transport.write(data)
 
 class ClientProtocolFactory(Factory):
@@ -61,11 +109,13 @@ class ServerProtocol(Protocol):
 		while len(c2s_queue) > 0:
 			data = c2s_queue.pop(0)
 			print "resume client send (len=%d)" % len(data)
+			parseData(data)
 			self.transport.write(data)
 
 	def dataReceived(self, data):
 		print "server send (len=%d)" % len(data)
 
+		parseData(data)
 		client_protocol.transport.write(data)
 
 class ServerProtocolFactory(ClientFactory):
